c17a379bfbecf98885b2d71061620be55ed0cb21
zkulphongku <zkulphong@ku.edu>
2018-03-23 15:11:12 -0500

    python 3 update

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def predict():
     prediction = ''
     prediction_val = 0
     prob_denominator = 0
-    for key, value in dTypes:
+    for key, value in dTypes.iteritems():
         prediction = key
         prediction_val = value
         prob_denominator = prob_denominator + value
